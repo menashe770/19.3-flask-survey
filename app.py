7d04aca4cbccdd03c6fa1ad964aa555a632abde2
Menashe <mborukhov770@gmail.com>
2023-05-01 15:11:41 -0600

    step 2

---
+++ app.py
@@ -0,0 +1,16 @@
+responses = []
+
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import surveys
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def start_survey():
+    survey = surveys["satisfaction"]
+    return render_template("survey_start.html", survey=survey)
