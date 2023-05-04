74ec234f6c2b5831ba7184316dd3731871912f22
Menashe <mborukhov770@gmail.com>
2023-05-04 17:06:52 -0600

    step 5

--- app.py
+++ app.py
@@ -15,8 +15,13 @@ def start_survey():
     return render_template("survey_start.html", survey=survey)
 
 
-@app.route("/questions/<int:qid>")
+@app.route("/question/<int:qid>")
 def show_question(qid):
+    responses = session.get(RESPONSES_KEY)
+
+    if len(responses) == len(survey.questions):
+        return redirect("/complete")
+
     question = survey.questions[qid]
     return render_template("questions.html", question=question)
 
@@ -28,3 +33,8 @@ def give_answers():
     responses.append(choice)
     session[RESPONSES_KEY] = responses
     return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/complete")
+def complete():
+    return render_template("complete.html")
